bc47ad3a17ad61346e200e340863c32284091512
Kristoffer Alquiza <kalquiza@gmail.com>
2021-01-12 15:29:25 -0800

    Create Person component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello, this is a new React app!</h1>
+        <p>This is really working!</p>
+        <Person />
       </div>
     );
+
+    // JSX is syntactic sugar that will compile into vanilla JavaScript
+    // return React.createElement('div', null, React.createElement('h1', {className: 'App'},'Hello, this is a new React app!'))
   }
 }
 
