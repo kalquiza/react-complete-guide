fb8e0b5004fd5ef2a13ccb9f28def7c4dfc14564
Kristoffer Alquiza <kalquiza@gmail.com>
2021-01-14 22:41:08 -0800

    Rollback app to class component
    Revert functional component app back to class component App.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,44 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: '???', age: 0 },
-      { name: '???', age: 1000 }
+      { name:'???', age: 1000}
     ],
-  });
-
-  const [otherState, setOtherState] = useState({
     otherState: 'some other value'
-  })
-
-  console.log(personsState, otherState);
-
-  const switchNameHandler = () => {
-    setPersonsState({
-      persons: [
-        { name: 'Kris', age: 0 },
-        { name: 'Nina', age: 1000 }
-      ]
-    })
   }
 
-  return (
-    <div className="App">
-      <h1>Hello, this is a new React app!</h1>
-      <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Reveal Names</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>Hobbies: Unknown</Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>Hobbies: Sleeping </Person>
-    </div>
-  );
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    
+    // Do not mutate state directly
+    // this.state.persons[0].name = 'deosn't work';
+
+    this.setState( {
+      persons: [
+        { name: 'Kris', age: 0 },
+        { name:'Nina', age: 1000}
+      ]
+    } )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, this is a new React app!</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Reveal Names</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Hobbies: Unknown</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Sleeping </Person>
+      </div>
+    );
+
+    // JSX is syntactic sugar that will compile into vanilla JavaScript
+    // return React.createElement('div', null, React.createElement('h1', {className: 'App'},'Hello, this is a new React app!'))
+  }
 }
 
-export default app;
+export default App;
