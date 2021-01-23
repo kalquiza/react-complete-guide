f1031b55d805cb13f2fe316049e452bfda021112
Kristoffer Alquiza <kalquiza@gmail.com>
2021-01-22 20:51:47 -0800

    Refactor project directory structure

--- base-syntax/src/App.js
+++ base-syntax/src/App.js
@@ -1,103 +1,56 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
   state = {
-    persons: [
-      { id: 'z57uhk', name: '???', age: 0 },
-      { id: 'jr5xkn', name: '???', age: 1000 }
-    ],
-    otherState: 'some other value',
-    showPersons: false
+    users: [
+      { username: '??????'},
+      { username: 'XXXXX'}
+    ]
   }
 
-  nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id;
-    });
-
-    // const person = Object.assign({}, this.state.persons[personIndex]);
-    const person = {
-      ...this.state.persons[personIndex]
-    };
-
-    person.name = event.target.value;
-
-    const persons = [...this.state.persons]
-    persons[personIndex] = person;
-    
-    this.setState({persons: persons});
-  }
-
-  deletePersonHandler = (personIndex) => {
-    // Update state immutably by copying the state
-    // use splice() or the spread operator with ES6
-    // const persons = this.state.persons.slice();
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons})
-  }
-
-  togglePersonsHandler = (event) => {
-    const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+  usernameChangedHandler = (event) => {
+    this.setState({
+      users: [
+        { username: event.target.value},
+        { username: 'XXXXX'}
+      ]
+    })
   }
 
   render() {
     const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
+      width: '60%',
+      margin: '16px auto',
+      padding: '16px'
     };
 
-    let persons = null;
-
-    if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={(event) => this.nameChangedHandler(event, person.id)}
-              />
-            )
-          })}
-          {/* <Person>
-            name={this.state.persons[0].name}
-            age={this.state.persons[0].age}
-            click={this.switchNameHandler.bind(this, 'Kristoff')}
-            changed={this.nameChangedHandler} >
-            Hobbies: Unknown</Person>
-          <Person
-            name={this.state.persons[1].name}
-            age={this.state.persons[1].age}
-            click={this.switchNameHandler.bind(this, 'Kristoffer')} >
-            Hobbies: Sleeping </Person> */}
-        </div>
-      );
-    }
-
     return (
-      <div className="App">
-        <h1>Hello, this is a new React app!</h1>
-        <p>This is really working!</p>
-        <button 
-          onClick={this.togglePersonsHandler}
-          style={style}> Reveal persons
-        </button>
-        {persons}
+      <div className="App" style={style}>
+        <UserInput
+        username={this.state.users[0].username}
+        changed={this.usernameChangedHandler}></UserInput>
+        <UserOutput
+        username={this.state.users[0].username}></UserOutput>
+        <UserOutput
+        username={this.state.users[1].username}></UserOutput>
+        <p>Assignment Instructions</p>
+        <ol>
+          <li>Create TWO new components: UserInput and UserOutput</li>
+          <li>UserInput should hold an input element, UserOutput two paragraphs</li>
+          <li>Output multiple UserOutput components in the App component (any paragraph texts of your choice)</li>
+          <li>Pass a username (of your choice) to UserOutput via props and display it there</li>
+          <li>Add state to the App component (=&gt; the username) and pass the username to the UserOutput component</li>
+          <li>Add a method to manipulate the state (=&gt; an event-handler method)</li>
+          <li>Pass the event-handler method reference to the UserInput component and bind it to the input-change event</li>
+          <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
+          <li>Add two-way-binding to your input (in UserInput) to also display the starting username</li>
+          <li>Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets</li>
+        </ol>
       </div>
-    );
-
-    // JSX is syntactic sugar that will compile into vanilla JavaScript
-    // return React.createElement('div', null, React.createElement('h1', {className: 'App'},'Hello, this is a new React app!'))
+  );
   }
 }
 
---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,104 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: 'z57uhk', name: '???', age: 0 },
+      { id: 'jr5xkn', name: '???', age: 1000 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons]
+    persons[personIndex] = person;
+    
+    this.setState({persons: persons});
+  }
+
+  deletePersonHandler = (personIndex) => {
+    // Update state immutably by copying the state
+    // use splice() or the spread operator with ES6
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  }
+
+  togglePersonsHandler = (event) => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
+              />
+            )
+          })}
+          {/* <Person>
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            click={this.switchNameHandler.bind(this, 'Kristoff')}
+            changed={this.nameChangedHandler} >
+            Hobbies: Unknown</Person>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Kristoffer')} >
+            Hobbies: Sleeping </Person> */}
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hello, this is a new React app!</h1>
+        <p>This is really working!</p>
+        <button 
+          onClick={this.togglePersonsHandler}
+          style={style}> Reveal persons
+        </button>
+        {persons}
+      </div>
+    );
+
+    // JSX is syntactic sugar that will compile into vanilla JavaScript
+    // return React.createElement('div', null, React.createElement('h1', {className: 'App'},'Hello, this is a new React app!'))
+  }
+}
+
+export default App;
