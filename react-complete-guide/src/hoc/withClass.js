3c1da10b8670067b0b64162a5b2cab7a80509b0c
Kristoffer Alquiza <kalquiza@gmail.com>
2021-02-01 17:35:41 -0800

    Pass props to withClass hoc component

--- react-complete-guide/src/hoc/withClass.js
+++ react-complete-guide/src/hoc/withClass.js
@@ -3,7 +3,7 @@ import React from 'react';
 const withClass = (WrappedConponent, className) => {
     return props => (
         <div className={className}>
-            <WrappedConponent />
+            <WrappedConponent {...props}/>
         </div>
     ); 
 };
