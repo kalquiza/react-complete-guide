fdb4ebc02c0dd820c57e54d366b9cc0e8a7fc73d
Kristoffer Alquiza <kalquiza@gmail.com>
2021-02-11 03:08:59 -0800

    Optimize Modal rendering
    Use shouldComponentUpdate to check if the props.show changes. Not using
    PureComponent because we are only checking props.show.

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Auxillary';
+import Aux from '../../../hoc/Auxillary/Auxillary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientsSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    //  For demonstration purposes only, otherwise OrderSummary can be a functional component
+    // componentWillUpdate() {
+    //     console.log('[OrderSummary] will update');
+    // }
+
+    render() {
+        const ingredientsSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return <li key={igKey}><
-                span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
             </li>
         })
 
-    return (
-        <Aux>
+        return (
+            <Aux>
             <h3>Your Order</h3>
             <p>A tasty burger with the following ingredients:</p>
             <ul>
                 {ingredientsSummary}
             </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType='Danger' clicked={this.props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    )
-};
+        );
+    }
+}
 
-export default orderSummary;
+export default OrderSummary;
