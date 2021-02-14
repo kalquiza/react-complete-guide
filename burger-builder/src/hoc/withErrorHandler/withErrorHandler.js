02b3d770eb9c2af153a3be9201e32cc24a776a08
Kristoffer Alquiza <kalquiza@gmail.com>
2021-02-14 00:23:47 -0800

    Optimise axios interceptors remove on unmount

--- burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-builder/src/hoc/withErrorHandler/withErrorHandler.js
@@ -21,6 +21,12 @@ const withErrorHandler = (WrappedComponent, axios) => {
             });
         }
 
+        componentWillUnmount() {
+            console.log('Will Unmount', this.reqInterceptor, this.resInterceptor);
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
+        }
+
         errorConfirmedHandler = () => {
             this.setState({error: null});
         }
