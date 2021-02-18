b1d1ba98adb642fb4733e700892affc4b7378222
Kristoffer Alquiza <kalquiza@gmail.com>
2021-02-17 21:24:26 -0800

    Add FullPost page using route parameters
    Use Route with a route parameter passed from a dynamic element and
    extract it using props.match. Navigate from the post element with
    props.history.push, or alternatively the Link component.

--- blog-posts/src/containers/Blog/Blog.js
+++ blog-posts/src/containers/Blog/Blog.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from '../Posts/Posts';
 import NewPost from '../NewPost/NewPost';
-
+import FullPost from '../FullPost/FullPost';
 class Blog extends Component {
     render () {
         return (
@@ -27,11 +27,14 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" render={() => <h1 style={{textAlign: "center"}}>The Blog Blog</h1>}/>
+                {/* <Route path="/" render={() => <h1 style={{textAlign: "center"}}>The Blog Blog</h1>}/>
                 <Route path="/" exact  render={() => <h2 style={{textAlign: "center"}}>Home</h2>}/>
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" render={() => <h2 style={{textAlign: "center"}}>New Post</h2>}/>
-                <Route path="/new-post" component={NewPost}/>
+                <Route path="/new-post" render={() => <h2 style={{textAlign: "center"}}>New Post</h2>}/> */}
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/posts/:id" component={FullPost}/>
+                </Switch>
             </div>
         );
     }
