bc6386f272772c70d8fe059414511774e7eda021
Kristoffer Alquiza <kalquiza@gmail.com>
2021-03-02 08:32:09 -0800

    Optimize reducer with immutable update patterns
    Update reducer to use immutable update patterns to update the store.
    Pass action payloada where necessary for state changes (resultsElId).

--- counter-redux/src/containers/Counter/Counter.js
+++ counter-redux/src/containers/Counter/Counter.js
@@ -34,6 +34,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={this.props.onAddValueCounter}  />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubtractValueCounter}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id} onClick={() => this.props.onDeleteResult(strResult.id)}>{strResult.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -41,7 +48,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     };
 };
 
@@ -50,7 +58,9 @@ const mapDispatchToProps = dispatch => {
         onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
         onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
         onAddValueCounter: () => dispatch({type: 'ADD', value: 5}),
-        onSubtractValueCounter: () => dispatch({type: 'SUBTRACT', value: 5})
+        onSubtractValueCounter: () => dispatch({type: 'SUBTRACT', value: 5}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: (id) => dispatch({type: 'DELETE_RESULT', resultElId: id})
     };
 };
 
--- counter-redux/src/store/reducer.js
+++ counter-redux/src/store/reducer.js
@@ -1,17 +1,50 @@
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            return {counter: state.counter + 1}
-        case 'DECREMENT':
-            return {counter: state.counter - 1}
+            // const newState = Object.assign({}, state);
+            // newState.counter = newState.counter + 1;
+            // return newState;
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case 'DECREMENT': 
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
         case 'ADD':
-            return {counter: state.counter + action.value}
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
         case 'SUBTRACT':
-            return {counter: state.counter - action.value}        
+            return {
+                ...state,
+                counter: state.counter - action.value
+            }        
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({
+                    id: new Date(),
+                    value: state.counter
+                })
+            }        
+        case 'DELETE_RESULT':
+            // const id = action.resultElIndex;
+            // const updatedArray = [...state.results];
+            // state.results.splice(action.resultElIndex, 1);
+            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+            return {
+                ...state,
+                results: updatedArray
+            }                    
         default:
             return state;
     }
