a958f0edb3576cd80b7f1bb99a4e9465f5c8a969
Kristoffer Alquiza <kalquiza@gmail.com>
2021-03-11 14:17:20 -0800

    Add utility function updateObject
    Create utility function for updating the state to create a leaner switch
    case in the reducer.

--- counter-redux/src/store/reducer/counter.js
+++ counter-redux/src/store/reducer/counter.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     counter: 0
@@ -6,32 +7,12 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.INCREMENT:
-            // const newState = Object.assign({}, state);
-            // newState.counter = newState.counter + 1;
-            // return newState;
-            return {
-                ...state,
-                counter: state.counter + 1
-            }
-        case actionTypes.DECREMENT: 
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
-        case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.value
-            }
-        case actionTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.value
-            }          
-        default:
-            return state;
+        case actionTypes.INCREMENT: return updateObject(state, {counter: state.counter + 1});
+        case actionTypes.DECREMENT: return updateObject(state, {counter: state.counter - 1});
+        case actionTypes.ADD: return updateObject(state, {counter: state.counter + action.value});
+        case actionTypes.SUBTRACT: return updateObject(state, {counter: state.counter - action.value});
     }
+    return state;
 };
 
 export default reducer;
--- counter-redux/src/store/reducer/result.js
+++ counter-redux/src/store/reducer/result.js
@@ -1,32 +1,21 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, {results: updatedArray});
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.STORE_RESULT:
-            // const updatedResponse = action.result * 2;
-            return {
-                ...state,
-                results: state.results.concat({
-                    id: new Date(),
-                    value: action.result
-                })
-            }        
-        case actionTypes.DELETE_RESULT:
-            // const id = action.resultElIndex;
-            // const updatedArray = [...state.results];
-            // state.results.splice(action.resultElIndex, 1);
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
-            }                    
-        default:
-            return state;
+        case actionTypes.STORE_RESULT: return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});  
+        case actionTypes.DELETE_RESULT: return deleteResult(state, action);
     }
+    return state;
 };
 
 export default reducer;
