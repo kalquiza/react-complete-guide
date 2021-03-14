ebd7529787ded9f123007cae623a72c06440c980
Kristoffer Alquiza <kalquiza@gmail.com>
2021-03-14 03:00:06 -0700

    Refactor burgerBuilder and order reducers
    Use utility functions to create a leaner switch case in the reducer.

--- burger-builder/src/store/reducers/order.js
+++ burger-builder/src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     orders: [],
@@ -7,58 +8,45 @@ const initialState = {
     purchased: false
 }
 
+const purchaseInit = (state, action) => {
+    return updateObject(state, {purchased: false, loading: false, error: false});
+};
+
+const purchaseBurgerStart = (state, action) => {
+    return updateObject(state, { loading: true});
+};
+
+const purchaseBurgerSuccess = (state, action) => {
+    const newOrder = updateObject(action.orderData, {id: action.orderId});
+    return updateObject(state, {loading: false, purchased: true, error: false, orders: state.orders.concat(newOrder)});
+};
+
+const purchaseBurgerFail = (state, action) => {
+    return updateObject(state, {loading: false, error: true});
+};
+
+const fetchOrdersStart = (state, action) => {
+    return updateObject(state, {loading: true, error: false});
+};
+
+const fetchOrdersSuccess = (state, action) => {
+    return updateObject(state, {loading: false, error: false, orders: action.orders});
+};
+
+const fetchOrdersFail = (state, action) => {
+    return updateObject( state, {loading: false, error: true} );
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.PURCHASE_INIT:
-            return {
-                ...state,
-                purchased: false,
-                loading: false,
-                error: false
-            }
-        case actionTypes.PURCHASE_BURGER_START:
-            return {
-                ...state,
-                loading: true
-            };
-        case actionTypes.PURCHASE_BURGER_SUCCESS:
-            const newOrder = {
-                ...action.orderData,
-                id: action.orderId
-            };
-            return {
-                ...state,
-                loading: false,
-                purchased: true,
-                error: false,
-                orders: state.orders.concat(newOrder)
-            };
-        case actionTypes.PURCHASE_BURGER_FAIL:
-            return {
-                ...state,
-                loading: false,
-                error: true
-            };
-        case actionTypes.FETCH_ORDERS_START:
-            return {
-                ...state,
-                loading: true,
-                error: false
-            };
-        case actionTypes.FETCH_ORDERS_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                error: false,
-                orders: action.orders
-            };
-        case actionTypes.FETCH_ORDERS_FAIL:
-            return {
-                ...state,
-                error: true
-            };
-        default:
-            return state;
+        case actionTypes.PURCHASE_INIT: return purchaseInit(state, action);
+        case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action);
+        case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action);
+        case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state, action);
+        case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state, action);
+        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action);
+        case actionTypes.FETCH_ORDERS_FAIL: return fetchOrdersFail(state, action);
+        default: return state;
     }
 };
 
