091fc953c463576bf3c3516ca66f30ce7e960baf
Kristoffer Alquiza <kalquiza@gmail.com>
2021-03-28 23:18:18 -0700

    Clean console.log statements
    Comment out logging out to the console. Leave commented out for
    reference.

--- burger-builder/src/store/actions/auth.js
+++ burger-builder/src/store/actions/auth.js
@@ -53,7 +53,7 @@ export const auth = (email, password, isSignup) => {
         }
         axios.post(url , authData)
             .then(res => {
-                console.log(res);
+                // console.log(res);
                 const expirationDate = new Date(new Date().getTime() + res.data.expiresIn * 1000);
                 localStorage.setItem('token', res.data.idToken);
                 localStorage.setItem('expirationDate', expirationDate);
@@ -62,7 +62,7 @@ export const auth = (email, password, isSignup) => {
                 dispatch(checkAuthTimeout(res.data.expiresIn));
             })
             .catch(err => {
-                console.log(err.response);
+                // console.log(err.response);
                 dispatch(authFail(err.response.data.error));
             });
     }
